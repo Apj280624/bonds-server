9621896a72a93cb5941a4ab4157ba949c128d16a
Amit Purohit <amitpurohit4747@gmail.com>
2023-08-08 19:36:44 +0530

    counterparty register route

--- src/routers/counterparty.router.js
+++ src/routers/counterparty.router.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const statusText = require("../utils/status_text");
 const Counterparty = require("../models/counterparty.model");
 
 router.get("/", async (req, res) => {
